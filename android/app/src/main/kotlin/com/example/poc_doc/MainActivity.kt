709db07674cbabcef357dece8b07b644a988d93b
pranish-ramteke <pranishramteke27@gmail.com>
2020-11-10 07:45:32 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/poc_doc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poc_doc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:poc_doc/sytody_app.dart';
+
+void main() {
+
+  runApp(MyHomePage());
+}
